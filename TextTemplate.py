dc26bf5e6a8b01cee945f1558a6cd34e90bcaa18
Maxwell Fusco <msf9542@g.rit.edu>
2020-07-20 14:58:20 -0400

    organizational

---
+++ TextTemplate.py
@@ -0,0 +1,10 @@
+"""
+Creates custom gui to copy custom text templates to clipboard
+Author: Maxwell Fusco
+Language: Python 3.8
+"""
+import tkinter
+
+class TextTemplate:
+    def __init(self):
+        pass
