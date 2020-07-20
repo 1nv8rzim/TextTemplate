3b66a46ae4b94aebb0d668cf11aa8328b19101c2
Maxwell Fusco <msf9542@g.rit.edu>
2020-07-20 16:28:57 -0400

    basic functionality with reading data files in cwd

--- TextTemplate.py
+++ TextTemplate.py
@@ -1,10 +1,38 @@
 """
-Creates custom gui to copy custom text templates to clipboard
+creates custom gui to copy custom text templates to clipboard
 Author: Maxwell Fusco
 Language: Python 3.8
 """
 import tkinter
+import os
 
 class TextTemplate:
     def __init__(self):
-        pass
+        self.root = tkinter.Tk()
+        self.responses = TextTemplate.generate_reponses()
+
+    @staticmethod
+    def generate_reponses():
+        responses = []
+        for filename in os.listdir(os.getcwd()):
+            if filename == 'TextTemplate.py':
+                continue
+            reponses.append(TextTemplate.generate_reponses(filename))
+        return responses.sort()
+
+    @staticmethod
+    def generate_reponse(filename):
+        file_tup = []
+        with open(filename) as file:
+            for i, line in enumerate(file):
+                if i == 0:
+                    file_tup.append(int(file.readline().strip()))
+                    file_tup.append(filename)
+                else:
+                    if len(file_tup) == 3:
+                        file_tup[2] += line
+                    else:
+                        file_tup.append(line)
+        return tuple(file_tup)
+        
+                        
